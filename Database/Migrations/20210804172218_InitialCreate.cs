931c45bba6cfd09abd5c51af2b821fe8950300ab
netotz <neto.otz@hotmail.com>
2021-08-04 12:28:03 -0500

    Add migration generated files

---
+++ Database/Migrations/20210804172218_InitialCreate.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CatGradoCarrera",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    originalId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatGradoCarrera", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CatInstitucion",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    idOriginal = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatInstitucion", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CatCarrera",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    originalId = table.Column<long>(type: "bigint", nullable: false),
+                    idGrado = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatCarrera", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CatCarrera_CatGradoCarrera_idGrado",
+                        column: x => x.idGrado,
+                        principalTable: "CatGradoCarrera",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CatRelacionInstitucionCarrera",
+                columns: table => new
+                {
+                    idInstitucion = table.Column<int>(type: "int", nullable: false),
+                    idCarrera = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatRelacionInstitucionCarrera", x => new { x.idInstitucion, x.idCarrera });
+                    table.ForeignKey(
+                        name: "FK_CatRelacionInstitucionCarrera_CatCarrera_idCarrera",
+                        column: x => x.idCarrera,
+                        principalTable: "CatCarrera",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CatRelacionInstitucionCarrera_CatInstitucion_idInstitucion",
+                        column: x => x.idInstitucion,
+                        principalTable: "CatInstitucion",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CatCarrera_idGrado",
+                table: "CatCarrera",
+                column: "idGrado");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CatRelacionInstitucionCarrera_idCarrera",
+                table: "CatRelacionInstitucionCarrera",
+                column: "idCarrera");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CatRelacionInstitucionCarrera");
+
+            migrationBuilder.DropTable(
+                name: "CatCarrera");
+
+            migrationBuilder.DropTable(
+                name: "CatInstitucion");
+
+            migrationBuilder.DropTable(
+                name: "CatGradoCarrera");
+        }
+    }
+}
